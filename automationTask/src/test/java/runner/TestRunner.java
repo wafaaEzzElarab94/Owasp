dd93befa13e09704f32e91f610141b67ce6f6b9a
wafaaEzzElarab94 <wafaa.ezzelarab@gmail.com>
2025-06-13 10:28:06 +0300

    fix issue

--- automationTask/src/test/java/runner/TestRunner.java
+++ automationTask/src/test/java/runner/TestRunner.java
@@ -2,12 +2,12 @@ package runner;
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
-import cucmber.api.junit.Cucumber;
+import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features="src/test/java/features",
-        glue={"src/test/java/stepDefinition","src/test/java/utility"},
-        plugin={"pretty","html:target/cucumber-html-report","json:cucumber.json"}
+        features ="src/test/java/features",
+        glue = {"src/test/java/stepDefinition","src/test/java/utility"},
+        plugin ={"pretty", "html:target/cucumber-html-report","json:cucumber.json"}
 )
 public class TestRunner {
 
